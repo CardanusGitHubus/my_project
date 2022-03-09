fe0ecf90bd09a1f394365f9ead690d5bae0e301e
CardanusGitHubus <lebedev.free@gmail.com>
2022-03-09 23:16:17 +0300

    add heroku files

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,9 +20,10 @@ from p_library import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('', views.books_list),
+    # path('', views.books_list),
     path('index/', views.index),
     path('index/book_increment/', views.book_increment),
     path('index/book_decrement/', views.book_decrement),
     path('publishers/', views.publishers),
+    path('', include('p_library.urls')),
 ]
--- p_library/views.py
+++ p_library/views.py
@@ -1,13 +1,33 @@
 from multiprocessing import context
-from re import template
+from re import I, template
+from sys import prefix
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
 
-from .models import Book, Author, Publisher
+from .models import Book
+from p_library.models import Author, Publisher
+from p_library.forms import AuthorForm, BookForm
+
+from django.forms import formset_factory
+from django.http.response import HttpResponseRedirect
 
 
 # Create your views here.
+class AuthorEdit(CreateView):
+    model = Author
+    form_class = AuthorForm
+    success_url = reverse_lazy('p_library:author_list')
+    template_name = 'p_library/author_edit.html'
+
+
+class AuthorList(ListView):
+    model = Author
+    template_name = 'p_library/authors_list.html'
+
+
 def books_list(request):
     books = Book.objects.all()
     return HttpResponse(books)
@@ -65,3 +85,45 @@ def book_decrement(request):
         return redirect('/index/')
     else:
         return redirect('/index/')
+
+# the function for a formset
+
+
+def author_create_many(request):
+    AuthorFormSet = formset_factory(AuthorForm, extra=2)
+    if request.method == 'POST':
+        author_formset = AuthorFormSet(
+            request.POST, request.FILES, prefix='authors')
+        if author_formset.is_valid():
+            for author_form in author_formset:
+                author_form.save()
+            return HttpResponseRedirect(reverse_lazy('p_library:author_list'))
+    else:
+        author_formset = AuthorFormSet(prefix='authors')
+        return render(request, 'p_library/manage_authors.html', {'author_formset': author_formset})
+
+
+def books_authors_create_many(request):
+    AuthorFormSet = formset_factory(AuthorForm, extra=2)
+    BookFormSet = formset_factory(BookForm, extra=2)
+    if request.method == 'POST':
+        author_formset = AuthorFormSet(
+            request.POST, request.FILES, prefix='authors')
+        book_formset = BookFormSet(request.POST, request.FILES, prefix='books')
+        if author_formset.is_valid() and book_formset.is_valid():
+            for author_form in author_formset:
+                author_form.save()
+            for book_form in book_formset:
+                book_form.save()
+            return HttpResponseRedirect(reverse_lazy('p_library:author_list'))
+    else:
+        author_formset = AuthorFormSet(prefix='authors')
+        book_formset = BookFormSet(prefix='books')
+    return render(
+        request,
+        'p_library/manage_books_authors.html',
+        {
+            'author_formset': author_formset,
+            'book_formset': book_formset,
+        }
+    )
