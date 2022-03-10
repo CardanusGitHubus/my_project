0393116cc335b8e0d5c215d54f93e8dc97df7d4f
CardanusGitHubus <lebedev.free@gmail.com>
2022-03-10 17:04:51 +0300

    venv start heroku try2

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fbe$ba5yr-5@&7vxuhaqb7l(x0mat)a@8c+$kl7ygk9zrjg$(p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'p_library',  # Наше приложение библиотеки
     'polls',  # test app from django docs tutorial
-    'django_extensions',  # https://django-extensions.readthedocs.io/en/latest/ (to see SQL queries in the terminal)
+    # https://django-extensions.readthedocs.io/en/latest/ (to see SQL queries in the terminal)
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -76,13 +79,15 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
